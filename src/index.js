0ac6e04746113fb8bc518c223fef0623c357ad24
Crysfel Villa <crysfel@bleext.com>
2018-08-17 12:25:19 -0500

    Rendering two textfields for the login

--- src/index.js
+++ src/index.js
@@ -3,58 +3,23 @@ function TextField(props) {
   return React.createElement(
     'label',
     null,
-    `${props.label}:`,
-    React.createElement(
-      'input',
-      {
-        onChange: props.onChange,
-        placeholder: props.placeholder,
-        type: 'text',
-        value: props.value,
+    `${props.label}: `,
+    React.createElement('input', {
+      name: props.name,
     })
   );
 }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      first: '',
-      last: '',
-    };
-  }
-
-  render() {
-    return React.createElement(
-      'div',
-      null,
-      React.createElement(
-        TextField,
-        {
-          label: 'First name',
-          name: 'firstname',
-          onChange: (event) => this.setState({ first: event.target.value }),
-          placeholder: 'What\'s your name?',
-          value: this.state.first,
-        }
-      ),
-      React.createElement(
-        TextField,
-        {
-          label: 'Last name',
-          name: 'lasttname',
-          onChange: (event) => this.setState({ last: event.target.value }),
-          placeholder: 'What\'s your last name?',
-          value: this.state.last,
-        }
-      )
-    );
-  }
+function App() {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(TextField, { label: 'Email', name: 'email' }),
+    React.createElement(TextField, { label: 'Password', name: 'password' })
+  );
 }
 
 ReactDOM.render(
   React.createElement(App, null),
   document.getElementById('root')
 );
-
